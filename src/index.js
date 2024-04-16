9c91b0739e58a8111782ecbbdacb99c37c5b422e
HeyFang <heyfang36@yahoo.com>
2024-04-16 19:31:03 +0530

    first slash command :)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token, clientId, guildId } = require('./config.json');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+
+// Create a new REST client
+const rest = new REST({ version: '10' }).setToken(token);
+
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
 
 // When the client is ready, run this code (only once).
 client.once(Events.ClientReady, readyClient => {
@@ -11,5 +24,42 @@ client.once(Events.ClientReady, readyClient => {
 });
 
 
+// Prints out the message content to the console
+/*
+client.on('messageCreate', (message) => {
+    console.log(message.author.tag + ': ' + message.content + '   (' + message.createdAt.toDateString() + ')');
+    }
+);
+*/
+
+
 // Log in to Discord with your client's token
 client.login(token);
+
+async function main (){
+    const commands = [
+        {
+            name: 'ping',
+            description: 'Replies with Pong!',
+        },
+        {
+            name: 'feng',
+            description: 'a watermelon.',
+        },
+    ];
+    try{
+
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+
+    } catch (error){
+        console.error(error);
+    }
+}
+
+main();
